c87399c13092df90a21fdf4a985938c74a9dbe0f
covalent <mail@lukehmcc.com>
2023-11-29 12:44:50 -0500

    fix synth error in game controller

--- dino-game/dino-game.srcs/sources_1/new/game_controller.v
+++ dino-game/dino-game.srcs/sources_1/new/game_controller.v
@@ -23,11 +23,11 @@
 module game_controller(
     input in_clk,
     input jump, // connect this to a switch for now. 
-    output reg [3:0] VGA_R,
-    output reg [3:0] VGA_G,
-    output reg [3:0] VGA_B,
-    output reg VGA_HS,
-    output reg VGA_VS
+    output [3:0] VGA_R,
+    output [3:0] VGA_G,
+    output [3:0] VGA_B,
+    output VGA_HS,
+    output VGA_VS
     );
     
     vga_controller v0(.in_clk(in_clk), .jump(jump), .VGA_R(VGA_R), .VGA_G(VGA_G), .VGA_B(VGA_B), .VGA_HS(VGA_HS), .VGA_VS(VGA_VS));
