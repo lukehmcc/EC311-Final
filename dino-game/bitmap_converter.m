ec4b9eb1b344c6204be58a60c2c54631d7742a5c
Angel Amaya <135993385+aamaya33@users.noreply.github.com>
2023-12-06 14:25:12 -0500

    Update bitmap_converter.m
    Added how to use the code and previous update was making a color bitmap instead of making it B/W

--- dino-game/bitmap_converter.m
+++ dino-game/bitmap_converter.m
@@ -1,3 +1,6 @@
+%Make sure to have image files in the matlab directory in order to execute this code. BMP file will be stored in local Matlab directory 
+
+
 % Read the image file into an array
 imageArray = imread('WizardFall.png');
 
