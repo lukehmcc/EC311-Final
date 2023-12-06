9d2aa52dba234b490035001ddd842a0dc2ee4944
Angel Amaya <135993385+aamaya33@users.noreply.github.com>
2023-12-06 14:23:07 -0500

    Update bitmap_converter.m

--- dino-game/bitmap_converter.m
+++ dino-game/bitmap_converter.m
@@ -1,17 +1,8 @@
-% Read the PNG image
-img = imread('Fireball.png');
+% Read the image file into an array
+imageArray = imread('WizardFall.png');
 
-% Convert the image to grayscale
-img_gray = rgb2gray(img);
-
-% Resize the image to 16x16
-img_resized = imresize(img_gray, [16 16]);
-
-% Convert the resized image to a bitmap image
-img_bitmap = imbinarize(img_gray); % Otsu's method is used by default
-
-% Write the bitmap image to a file
-imwrite(img_bitmap, '/Users/aamaya3/Desktop/311 Sprites/Fireball.bmp');
+% Write the array to a bitmap file
+imwrite(imageArray, 'WizardFall.bmp', 'bmp');
 
 % Display the bitmap image
-%imshow(img_bitmap);
+imshow('WizardFall.bmp');
